58cb9df01c615bb9c5fad50abb4e512276403149
brownieholiday <sdshowalter@yahoo.com>
2016-07-31 21:22:38 -0700

    added case studies archive and individual case studies pages

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying case studies
+ * The template for displaying the case studies archive page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -17,48 +17,37 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); 
-
 				$size = "full";
 				$services = get_field('services');
 				$client = get_field('client');
-				$site_link = get_field('site_link');
 				$image_1 = get_field('image_1');
-				$image_2 = get_field('image_2');
-				$image_3 = get_field('image_3'); ?>
-				
-				<article class="case-study">
+			?>
+
+			<article class="case-study">
 					<aside class="case-study-sidebar">
-						<h2><php the_title(); ?></h2>
-						<h5><?php echo $services; ?></h5>
-						<h6>Client: <?php echo $client; ?></h6>
+						<h2><a href="<?php the_permalink(); ?>"><?php echo $client; ?></a></h2>
+						<h6><?php echo $services; ?></h6>
+						
 					
 
-						<?php the_content(); ?>
+						<?php the_excerpt(); ?>
 
-						<p><strong><a href="<?php echo $site_link; ?>">Site Link</a></strong></p>
+						<p><strong><a href="<?php the_permalink(); ?>">View Project &rsaquo;</a></strong></p>
 
 					</aside>
 
 					<div class="case-study-images">
+						<a href="<?php the_permalink(); ?>">
 						<?php if($image_1) { 
 							echo wp_get_attachment_image( $image_1, $size ); 
-						 } ?>
-						<?php if($image_2) { 
-							echo wp_get_attachment_image( $image_2, $size ); 
-						 } ?>
-				 		<?php if($image_3) { 
-				 			echo wp_get_attachment_image( $image_3, $size ); 
-				 		 } ?>	
+						 } ?>	
+						</a>
 				 	</div>			 		
 				 </article>
-
+				
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
-
-
-
-<div class="case-study-images"> </div>
